fbee862d15ffb9ef07f552af76f223231490536b
RobertBrooks <RobertBrooks1613@outlook.com>
2024-12-02 16:09:58 -0500

    script updated

--- main.py
+++ main.py
@@ -14,7 +14,18 @@ def word_count():
     '''Returns the books word length'''
     return len(read_book("books/frankenstein.txt",words=True))
 
-print(word_count())
+def char_count():
+    text = read_book("books/frankenstein.txt")
+    char_count = {}
+    for char in text.lower():
+        if char not in char_count and char.isalpha():
+            char_count = char_count | {char: 1}
+        if char in char_count:
+            char_count[char] += 1
+    for letter in char_count:        
+        print(f"The '{letter}' character was found {char_count[letter]} times")
+
+char_count()
 
 
 
